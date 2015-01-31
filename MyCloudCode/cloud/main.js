a41c577ecf1a5e8aac1b4a02e9066579030a7f3b
Viral Patel <pviral0225@gmail.com>
2015-01-31 04:26:56 -0500

    add signUpNewUser function

--- MyCloudCode/cloud/main.js
+++ MyCloudCode/cloud/main.js
@@ -4,3 +4,28 @@
 Parse.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
 });
+
+Parse.Cloud.define("newUserSignUp", function(request, response){
+    var user = new Parse.User();
+    user.set("username", request.param.username);
+    user.set("password", request.param.password);
+    user.set("email", request.param.email);
+    user.set("tagLine", request.param.tagLine);
+    user.set("authorName", request.param.username);
+    user.set("followers", []);
+    user.set("following", []);
+    user.set("posts", []);
+    user.set("postsLiked", []);
+    user.set("accountSettings", request.param.accountSettings);
+    user.set("profilePic", request.param.profilePic);
+
+    user.signUp(null, {
+      success: function(user) {
+        // Hooray! Let them use the app now.
+      },
+      error: function(user, error) {
+        // Show the error message somewhere and let the user try again.
+        alert("Error: " + error.code + " " + error.message);
+      }
+    });
+});
