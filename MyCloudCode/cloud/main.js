79507e88ef397dfbc344cdd42c3f77a57d360b03
Viral Patel <pviral0225@gmail.com>
2015-01-31 04:49:28 -0500

    added createNewPost function

--- MyCloudCode/cloud/main.js
+++ MyCloudCode/cloud/main.js
@@ -1,9 +1,5 @@
 
 // Use Parse.Cloud.define to define as many cloud functions as you want.
-// For example:
-Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
-});
 
 Parse.Cloud.define("newUserSignUp", function(request, response){
     var user = new Parse.User();
@@ -28,4 +24,33 @@ Parse.Cloud.define("newUserSignUp", function(request, response){
         alert("Error: " + error.code + " " + error.message);
       }
     });
+});
+
+Parse.Cloud.define("createNewPost", function(request, response){
+    var post = new Parse.Post();
+    
+    post.set("Title", request.param.title);
+    post.set("displayedAuthorName", request.param.displayName);
+    post.set("coverImg", request.param.converImg);
+    post.set("createdAt", Date.now);
+    post.set("updatedAt", Date.now);
+    post.set("tags", request.param.tags);
+    post.set("likes", []);
+    post.set("views", []);
+    post.set("prompt", request.param.prompt);
+    post.set("wordCount", request.param.wordCount);
+    post.set("charCount", request.param.wordCount);
+    post.set("textContent", request.param.textContent);
+    
+    post.save(null, {
+  success: function(post) {
+    // Execute any logic that should take place after the object is saved.
+    alert('New object created with objectId: ' + post.id);
+  },
+  error: function(post, error) {
+    // Execute any logic that should take place if the save fails.
+    // error is a Parse.Error with an error code and message.
+    alert('Failed to create new object, with error code: ' + error.message);
+  }
+});
 });
